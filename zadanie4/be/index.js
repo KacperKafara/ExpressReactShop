287bb415fed4e56c16e40a1eb7e28c6ee087a9c7
Kacper Kafara <kacperkafara18@gmail.com>
2023-12-16 19:16:54 +0100

    closing db connection

--- zadanie4/be/index.js
+++ zadanie4/be/index.js
@@ -1,9 +1,10 @@
 import run from './db/db_init.js';
-run();
+await run();
 
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import statusController from './controllers/statusController.js';
 import categoryController from './controllers/categoryController.js';
 
@@ -14,6 +15,13 @@ const PORT = 3000;
 app.use('/status', statusController);
 app.use('/categories', categoryController);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+})
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        mongoose.connection.close();
+        console.log("DB connection closed.");
+    })
 })
