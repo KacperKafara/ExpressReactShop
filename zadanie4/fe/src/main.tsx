9539aa253e4c75766db391bd74c183cffa4cfbc1
Kacper Kafara <kacperkafara18@gmail.com>
2023-12-26 23:55:44 +0100

    App.tsx is no more

--- zadanie4/fe/src/main.tsx
+++ zadanie4/fe/src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import React from 'react'
-import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { RoutesComponent } from './router/Routes/index'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RoutesComponent />
+    </BrowserRouter>
   </React.StrictMode>,
 )
